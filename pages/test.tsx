16dc7f8a839ee6d8a42ac1ad237f6f43c9367f0a
Kevin Xu <kevinxu16@gmail.com>
2025-11-08 00:20:28 +0800

    First pass updated hotspot

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,149 +1,146 @@
-import { useState, useEffect } from "react";
-import StickyBox from "@/src/components/StickyBox";
-import Header from "@/src/components/Header";
-import Footer from "@/src/components/Footer";
-import {
-  Box,
-  Grid,
-  Typography,
-  Fade,
-  Stack,
-  useMediaQuery,
-  useTheme,
-  ImageList,
-  ImageListItem,
-  ImageListItemBar,
-} from "@mui/material";
-import { Row, Column } from "../src/components/Layout";
-import { FullWidthSection } from "@/src/components/Section";
-import { PADDING_X_SECTION, PADDING_X_MOBILE } from "@/src/constants";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import CloseIcon from "@mui/icons-material/Close";
 import { Masonry } from "@mui/lab";
+import { Box, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import { useState } from "react";
+
+import Footer from "@/src/components/Footer";
+import Header from "@/src/components/Header";
+import { FullWidthSection } from "@/src/components/Section";
+import StickyBox from "@/src/components/StickyBox";
+import { PADDING_X_SECTION, PADDING_X_MOBILE } from "@/src/constants";
 import ExploreSection from "@/src/pages/home/ExploreSection";
 
-const DESKTOP_EXPLORE_PROJECTS_SECTION_HEIGHT = "800px";
-const DESKTOP_SPACING = 2;
+import { Row, Column } from "../src/components/Layout";
 
 export default function HomePage({}) {
   return (
     <>
       <Header sticky={false} />
 
-      {/* <FullWidthSection
+      <FullWidthSection
         sx={{
-          alignItems: "center",
-          backgroundColor: "#f1eeed",
           px: {
-            xs: 0,
-            md: 2,
+            xs: PADDING_X_MOBILE,
+            md: PADDING_X_SECTION,
           },
           py: {
-            xs: 0,
-            md: 2,
+            xs: 2,
+            md: PADDING_X_SECTION,
           },
         }}
       >
-        <ExploreSection images={mockData.explore} />
-      </FullWidthSection> */}
+        <HotspotImage
+          image={mockData.hotspotImage}
+          hotspots={mockData.hotspots}
+        />
+      </FullWidthSection>
 
       <Footer />
     </>
   );
 }
 
+interface Hotspot {
+  title: string;
+  description: string;
+  position: {
+    x: number;
+    y: number;
+  };
+}
+
+interface HotspotImageProps {
+  image: string;
+  hotspots: Hotspot[];
+}
+
+function HotspotImage({ image, hotspots }: HotspotImageProps) {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <>
+      <Row
+        sx={{
+          position: "relative",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          component="img"
+          src={image}
+          alt="Interactive"
+          sx={{
+            objectFit: "cover",
+            display: "block",
+          }}
+        />
+
+        {/* this is the hotspot */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: "100px",
+            left: "100px",
+            // transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Box
+            onClick={() => setIsActive(!isActive)}
+            sx={{
+              width: 12,
+              height: 12,
+              borderRadius: "50%",
+              backgroundColor: "gray",
+              border: "2px solid white",
+              cursor: "pointer",
+            }}
+          />
+        </Box>
+
+        {/* this is the tooltip that gets displayed */}
+        {isActive && (
+          <Box
+            sx={{
+              position: "absolute",
+              top: "150px",
+              left: "250px",
+              borderRadius: "2px",
+              maxWidth: "500px",
+              backgroundColor: "#f1eeed",
+              padding: 2,
+            }}
+          >
+            <Column gap={1}>
+              <Typography sx={{ fontStyle: "italic" }}>Cabinets</Typography>
+              <Typography color="text.secondary">
+                For the opposite wall, we wanted it to be a softer Gothic style
+                while still being dramatic, so we chose the elegant
+                floor-to-ceiling Escada cabinets and matched them to the greige
+                walls to make the space look even taller
+              </Typography>
+            </Column>
+          </Box>
+        )}
+      </Row>
+    </>
+  );
+}
+
 const mockData = {
-  featuredProject: [
+  hotspotImage: "/IMG_0965.jpeg",
+  hotspots: [
     {
-      src: "/IMG_0008.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "/IMG_0002_landscape.jpeg",
-      title: "Hand-painted umbrellas",
-      subtitle: "Chiang Mai, Thailand",
-    },
-    {
-      src: "/IMG_0017.jpeg",
-      title: "Watercolor scroll",
-      subtitle: "Guangzhou, China",
-    },
-  ],
-  services: [
-    {
-      numberText: "04 / 04",
-      title: "Content Creation",
-      bg: "#bab2a3",
+      title: "Cabinets",
       description:
-        "Our team produces personalized content and original materials that share your work with clarity and authenticity across social platforms and via email marketing. Designed to engage, inform, and convert, every communication is crafted to resonate with your audience.",
-    },
-    {
-      numberText: "03 / 04",
-      title: "Brand Strategy",
-      bg: "#c8c0b3",
-      description:
-        "Our team produces personalized content and original materials that share your work with clarity and authenticity across social platforms and via email marketing. Designed to engage, inform, and convert, every communication is crafted to resonate with your audience.",
-    },
-    {
-      numberText: "02 / 04",
-      title: "Video Strategy",
-      bg: "#d9d3ca",
-      description:
-        "Our team produces personalized content and original materials that share your work with clarity and authenticity across social platforms and via email marketing. Designed to engage, inform, and convert, every communication is crafted to resonate with your audience.",
-    },
-    {
-      numberText: "01 / 04",
-      title: "Video Production",
-      bg: "#f1eeed",
-      description:
-        "Our team produces personalized content and original materials that share your work with clarity and authenticity across social platforms and via email marketing. Designed to engage, inform, and convert, every communication is crafted to resonate with your audience.",
-    },
-  ],
-  explore: [
-    {
-      src: "/IMG_0002_landscape.jpeg",
-      title: "Hand-painted umbrellas",
-      subtitle: "Chiang Mai, Thailand",
-    },
-    {
-      src: "/IMG_0004.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "/IMG_0008.jpeg",
-      title: "Watercolor scroll",
-      subtitle: "Guangzhou, China",
-    },
-    {
-      src: "https://soho-spaces.com/assets/dark-academia/IMG_0021.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "https://soho-spaces.com/assets/muted-mediterranean/IMG_0013.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "https://soho-spaces.com/assets/muted-mediterranean/IMG_0011.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "https://soho-spaces.com/assets/muted-mediterranean/IMG_3699.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "https://soho-spaces.com/assets/east-meets-west/IMG_9480.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
-    },
-    {
-      src: "https://soho-spaces.com/assets/east-meets-west/IMG_9474.jpeg",
-      title: "Brass towel bar",
-      subtitle: "Bangkok, Thailand",
+        "For the opposite wall, we wanted it to be a softer Gothic style while still being dramatic, so we chose the elegant floor-to-ceiling Escada cabinets and matched them to the greige walls to make the space look even taller. ",
+      position: {
+        x: 50,
+        y: 50,
+      },
     },
   ],
 };
