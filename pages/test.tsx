f12792dcde0c5355b3e74fdba0f82d7c71875ef1
Kevin Xu <kevinxu16@gmail.com>
2025-11-08 20:55:47 -0800

    Checkpoint - refactor with top-left working

--- pages/test.tsx
+++ pages/test.tsx
@@ -47,10 +47,8 @@ export default function HomePage({}) {
 interface Hotspot {
   title: string;
   description: string;
-  position: {
-    x: number;
-    y: number;
-  };
+  x: number;
+  y: number;
 }
 
 interface HotspotImageProps {
@@ -102,12 +100,14 @@ function HotspotImage({ image, hotspots }: HotspotImageProps) {
   return (
     <Row sx={{ justifyContent: "center" }}>
       <Box sx={{ position: "relative", display: "inline-block" }}>
+        {/* Background Image */}
         <Box
           ref={imgRef}
           component="img"
           src={image}
           alt="Interactive"
           sx={{
+            zIndex: 0,
             objectFit: "cover",
             display: "block",
             position: "relative",
@@ -140,14 +140,9 @@ function HotspotImage({ image, hotspots }: HotspotImageProps) {
 
         {isActive && (
           <>
-            {/* Vertical line */}
             {renderVerticalLine({ hotspot, image: imageSize })}
-
-            {/* Horizontal line */}
-            {renderHorizontalLine({ hotspot, image: imageSize })}
-
-            {/* Tooltip Card */}
-            {renderTooltipCard({ hotspot, image: imageSize })}
+            {/* {renderHorizontalLine({ hotspot, image: imageSize })} */}
+            {/* {renderTooltipCard({ hotspot, image: imageSize })} */}
           </>
         )}
       </Box>
@@ -156,97 +151,147 @@ function HotspotImage({ image, hotspots }: HotspotImageProps) {
 }
 
 function renderVerticalLine({ hotspot, image }: Props) {
-  const verticalLineTop = hotspot.y + HOTSPOT_SIZE / 2;
-  const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
-  return (
+  // TOP TO BOTTOM
+  if (isTop({ hotspot, image })) {
+    const verticalLineTop = hotspot.y + HOTSPOT_SIZE / 2;
+    const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
+    return (
+      <Box
+        sx={{
+          position: "absolute",
+          top: verticalLineTop,
+          left: verticalLineLeft,
+          width: LINE_THICKNESS,
+          height: 0,
+          backgroundColor: "#f1eeed",
+          zIndex: 1,
+          animation: "grow-vertical 0.2s ease-out forwards",
+          "@keyframes grow-vertical": {
+            to: { height: VERTICAL_LINE_HEIGHT },
+          },
+        }}
+      />
+    );
+  } else {
+    console.log("rendering vertical line from bottom");
+    const verticalLineTop = hotspot.y + HOTSPOT_SIZE / 2;
+    const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
     <Box
       sx={{
         position: "absolute",
-        top: verticalLineTop,
+        bottom: `calc(${image.height - hotspot.y}px - ${HOTSPOT_SIZE / 2}px)`, // fixed at hotspot
         left: verticalLineLeft,
         width: LINE_THICKNESS,
-        height: 0,
+        height: 0, // start collapsed
         backgroundColor: "#f1eeed",
         zIndex: 1,
-        animation: "grow-vertical 0.2s ease-out forwards",
-        "@keyframes grow-vertical": {
-          to: { height: VERTICAL_LINE_HEIGHT },
+        animation: "grow-vertical-up 0.2s ease-out forwards",
+        "@keyframes grow-vertical-up": {
+          to: { height: VERTICAL_LINE_HEIGHT }, // grows upwards
         },
       }}
-    />
-  );
+    />;
+  }
 }
 
 function renderHorizontalLine({ hotspot, image }: Props) {
-  const verticalLineTop = hotspot.y + HOTSPOT_SIZE / 2;
-  const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
-  const horizontalLineTop = verticalLineTop + VERTICAL_LINE_HEIGHT;
-  const horizontalLineLeft = verticalLineLeft;
-  return (
-    <Box
-      sx={{
-        position: "absolute",
-        top: horizontalLineTop,
-        left: horizontalLineLeft,
-        width: 0,
-        height: LINE_THICKNESS,
-        backgroundColor: "#f1eeed",
-        zIndex: 1,
-        animation: "grow-horizontal 0.2s ease-out forwards",
-        animationDelay: "0.2s",
-        "@keyframes grow-horizontal": {
-          to: { width: HORIZONTAL_LINE_WIDTH },
-        },
-      }}
-    />
-  );
+  // LEFT TO RIGHT
+  if (isLeft({ hotspot, image })) {
+    const verticalLineTop = hotspot.y + HOTSPOT_SIZE / 2;
+    const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
+    const horizontalLineTop = verticalLineTop + VERTICAL_LINE_HEIGHT;
+    const horizontalLineLeft = verticalLineLeft;
+    return (
+      <Box
+        sx={{
+          position: "absolute",
+          top: horizontalLineTop,
+          left: horizontalLineLeft,
+          width: 0,
+          height: LINE_THICKNESS,
+          backgroundColor: "#f1eeed",
+          zIndex: 1,
+          animation: "grow-horizontal 0.2s ease-out forwards",
+          animationDelay: "0.2s",
+          "@keyframes grow-horizontal": {
+            to: { width: HORIZONTAL_LINE_WIDTH },
+          },
+        }}
+      />
+    );
+  }
 }
 
 function renderTooltipCard({ hotspot, image }: Props) {
-  const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
-  const horizontalLineLeft = verticalLineLeft;
-  const tooltipTop = hotspot.y;
-  const tooltipLeft = horizontalLineLeft + HORIZONTAL_LINE_WIDTH;
-  return (
-    <Box
-      sx={{
-        position: "absolute",
-        top: tooltipTop,
-        left: tooltipLeft,
-        borderRadius: "4px",
-        maxWidth: 500,
-        backgroundColor: "#f1eeed",
-        padding: 2,
-        boxShadow: "0 6px 20px rgba(0, 0, 0, 0.15)",
-        zIndex: 2,
-        opacity: 0,
-        transform: "translateY(10px)",
-        animation: "fade-in-tooltip 0.2s ease-out forwards",
-        animationDelay: "0.4s",
-        "@keyframes fade-in-tooltip": {
-          to: { opacity: 1, transform: "translateY(0)" },
-        },
-      }}
-    >
-      <Column gap={1}>
-        <Typography sx={{ fontStyle: "italic" }}>{hotspot.title}</Typography>
-        <Typography color="text.secondary">{hotspot.description}</Typography>
-      </Column>
-    </Box>
-  );
+  if (isTop({ hotspot, image }) && isLeft({ hotspot, image })) {
+    const verticalLineLeft = hotspot.x + HOTSPOT_SIZE / 2;
+    const horizontalLineLeft = verticalLineLeft;
+    const tooltipTop = hotspot.y;
+    const tooltipLeft = horizontalLineLeft + HORIZONTAL_LINE_WIDTH;
+    return (
+      <Box
+        sx={{
+          position: "absolute",
+          top: tooltipTop,
+          left: tooltipLeft,
+          borderRadius: "4px",
+          maxWidth: 500,
+          backgroundColor: "#f1eeed",
+          padding: 2,
+          boxShadow: "0 6px 20px rgba(0, 0, 0, 0.15)",
+          zIndex: 2,
+          opacity: 0,
+          transform: "translateY(10px)",
+          animation: "fade-in-tooltip 0.2s ease-out forwards",
+          animationDelay: "0.4s",
+          "@keyframes fade-in-tooltip": {
+            to: { opacity: 1, transform: "translateY(0)" },
+          },
+        }}
+      >
+        <Column gap={1}>
+          <Typography sx={{ fontStyle: "italic" }}>{hotspot.title}</Typography>
+          <Typography color="text.secondary">{hotspot.description}</Typography>
+        </Column>
+      </Box>
+    );
+  }
 }
 
 const mockData = {
   hotspotImage: "/IMG_0965.jpeg",
   hotspots: [
+    // Top Left
     {
       title: "Cabinets",
       description:
         "For the opposite wall, we wanted it to be a softer Gothic style while still being dramatic, so we chose the elegant floor-to-ceiling Escada cabinets and matched them to the greige walls to make the space look even taller. ",
-      position: {
-        x: 50,
-        y: 50,
-      },
+      x: 100,
+      y: 100,
+    },
+    // Bottom Left
+    {
+      title: "Cabinets",
+      description:
+        "For the opposite wall, we wanted it to be a softer Gothic style while still being dramatic, so we chose the elegant floor-to-ceiling Escada cabinets and matched them to the greige walls to make the space look even taller. ",
+      x: 100,
+      y: 700,
+    },
+    // Top Right
+    {
+      title: "Cabinets",
+      description:
+        "For the opposite wall, we wanted it to be a softer Gothic style while still being dramatic, so we chose the elegant floor-to-ceiling Escada cabinets and matched them to the greige walls to make the space look even taller. ",
+      x: 1100,
+      y: 100,
+    },
+    // Bottom Right
+    {
+      title: "Cabinets",
+      description:
+        "For the opposite wall, we wanted it to be a softer Gothic style while still being dramatic, so we chose the elegant floor-to-ceiling Escada cabinets and matched them to the greige walls to make the space look even taller. ",
+      x: 1100,
+      y: 700,
     },
   ],
 };
