aa587353f58cb77315523fdbf0fdbdcc935322d6
Kevin Xu <kevinxu16@gmail.com>
2025-12-14 21:57:17 -0500

    Explore > hook up Lightbox

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,15 +1,24 @@
 import { Box } from "@mui/material";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import { useState } from "react";
 import PhotoAlbum from "react-photo-album";
-
 import "react-photo-album/styles.css";
+import Lightbox from "yet-another-react-lightbox";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+
 import Footer from "@/src/components/Footer";
 import Header from "@/src/components/Header";
 import PageMeta from "@/src/components/PageMeta";
 import { PAGES } from "@/src/constants";
 import { useIsMobile } from "@/src/hooks/useIsMobile";
 import { client } from "@/src/sanity/client";
+
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 interface SanityImage {
   asset: {
@@ -38,9 +47,12 @@ interface ExplorePhoto {
 const builder = imageUrlBuilder(client);
 const urlFor = (source: SanityImageSource) => builder.image(source);
 
-const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
+const BREAKPOINTS = [1080, 640, 384, 256, 128, 96, 64, 48];
+const LIGHTBOX_WIDTH = 2400;
+const DEFAULT_IMAGE_WIDTH = 1440;
 
 export default function ExplorePage({ photos }: { photos: ExplorePhoto[] }) {
+  const [index, setIndex] = useState(-1);
   const isMobile = useIsMobile();
   const mergedPhotos = photos.map(transformPhoto);
 
@@ -74,8 +86,20 @@ export default function ExplorePage({ photos }: { photos: ExplorePhoto[] }) {
           layout="columns"
           columns={isMobile ? 2 : 3}
           spacing={isMobile ? 8 : 16}
+          onClick={({ index }) => setIndex(index)}
         />
       </Box>
+
+      <Lightbox
+        slides={mergedPhotos.map((p) => ({
+          src: isMobile ? p.src : p.lightboxSrc, // use high-res only on desktop
+          //   title: p.title, // used for captions
+        }))}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
 
       <Footer />
     </>
@@ -86,10 +110,11 @@ function transformPhoto(photo: ExplorePhoto) {
   const metadata = photo.image.asset.metadata;
   const { width: originalWidth, height: originalHeight } = metadata.dimensions;
 
-  const mainWidth = 1440;
-  const mainHeight = Math.round((originalHeight / originalWidth) * mainWidth);
+  const mainHeight = Math.round(
+    (originalHeight / originalWidth) * DEFAULT_IMAGE_WIDTH
+  );
 
-  const srcSet = breakpoints.map((w) => {
+  const srcSet = BREAKPOINTS.map((w) => {
     const h = Math.round((originalHeight / originalWidth) * w);
     return {
       src: urlFor(photo.image).width(w).url()!,
@@ -103,13 +128,16 @@ function transformPhoto(photo: ExplorePhoto) {
     _id: photo._id,
     title: photo.title,
     labels: photo.labels,
-    priority: photo.priority,
     project: photo.project,
-    // PhotoAlbum data
-    src: urlFor(photo.image).width(mainWidth).url()!,
+
+    // Photo Album metadata
+    src: urlFor(photo.image).width(DEFAULT_IMAGE_WIDTH).url()!,
     srcSet,
-    width: mainWidth,
+    width: DEFAULT_IMAGE_WIDTH,
     height: mainHeight,
+
+    // Lightbox
+    lightboxSrc: urlFor(photo.image).width(LIGHTBOX_WIDTH).url(),
   };
 }
 
