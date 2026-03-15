bc7d0611c58d8cf73cc8c8e42814ba6f309dcc25
Kevin Xu <kevinxu16@gmail.com>
2026-03-15 09:25:15 -0400

    Mobile before / after > bump react compare slider version

--- src/pages/project/BeforeAfterSection.tsx
+++ src/pages/project/BeforeAfterSection.tsx
@@ -54,7 +54,7 @@ export default function BeforeAfterSection({ items }: BeforeAfterSectionProps) {
                 }}
               >
                 <ReactCompareSlider
-                  position={20}
+                  defaultPosition={20}
                   style={{ width: "100%", height: "100%" }}
                   itemOne={
                     <ReactCompareSliderImage
